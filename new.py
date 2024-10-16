7dfec93e246b8973ca305fedf25769b123f899e7
Malihashahi <maliha.shahi.03@gmail.com>
2024-10-16 22:24:28 +0330

    start machine learning

--- new.py
+++ new.py
@@ -1,15 +1,7 @@
-import tkinter as tk
+import numpy
 
-root= tk.Tk()
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
+x = numpy.mean(speed)
 
-def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='blue', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-    
-button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
-
-root.mainloop()
+print(x)
