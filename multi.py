501804eeaedee81121d43fb101974c087108b8e0
Malihashahi <maliha.shahi.03@gmail.com>
2024-10-28 22:27:47 +0330

    logisticRegression in mL

--- multi.py
+++ multi.py
@@ -1,15 +1,13 @@
-import pandas
+import numpy
 from sklearn import linear_model
 
-df = pandas.read_csv("data.csv")
+#Reshaped for Logistic function.
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
-X = df[['Weight', 'Volume']]
-y = df['CO2']
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
 
-regr = linear_model.LinearRegression()
-regr.fit(X, y)
-
-#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
-predictedCO2 = regr.predict([[2300, 1300]])
-
-print(predictedCO2)
+#predict if tumor is cancerous where the size is 3.46mm:
+predicted = logr.predict(numpy.array([3.46]).reshape(-1,1))
+print(predicted)
