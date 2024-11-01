f67d352ddc3db98ade815cc5e697871114cad2ac
Malihashahi <maliha.shahi.03@gmail.com>
2024-11-01 22:54:40 +0330

    KNN  simple ex

--- multi.py
+++ multi.py
@@ -1,13 +1,8 @@
-import numpy
-from sklearn import linear_model
+import matplotlib.pyplot as plt
 
-#Reshaped for Logistic function.
-X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
-y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
 
-logr = linear_model.LogisticRegression()
-logr.fit(X,y)
-
-#predict if tumor is cancerous where the size is 3.46mm:
-predicted = logr.predict(numpy.array([3.46]).reshape(-1,1))
-print(predicted)
+plt.scatter(x, y, c=classes)
+plt.show()
