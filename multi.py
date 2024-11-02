e7e17a5a6c63bebac7b8b01338fcc4657a907db6
Malihashahi <maliha.shahi.03@gmail.com>
2024-11-02 22:19:27 +0330

    work with co2 dataset

--- multi.py
+++ multi.py
@@ -1,8 +1,43 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn import linear_model
+import sklearn.metrics as sm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt 
 
-x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
 
-plt.scatter(x, y, c=classes)
+df = pd.read_csv('co2.csv')
+
+print(df.head(10))
+print(df.describe())
+
+sns.countplot(x='out1', data=df)
+
+plt.subplots(figsize=(9, 9))
+sns.heatmap(df.corr(), annot=True)
+
+
+x = df.drop("out1", axis=1)
+x = x.drop("fuelcomb", axis=1)
+x = x.drop("cylandr", axis=1)
+y = df.out1
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+reg_linear= linear_model.LinearRegression()
+
+reg_linear.fit(X_train, y_train)
+
+y_test_pred = reg_linear.predict(X_test)
+
+print(X_test.size)
+
+test=np.array([[3]])
+
+khoroji = reg_linear.predict(test)
+
+print(khoroji)
+
+
 plt.show()
