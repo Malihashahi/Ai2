aaf837b9d222f46534a309604cf2839fc73dd0dd
Malihashahi <maliha.shahi.03@gmail.com>
2024-11-04 22:06:58 +0330

    percentile  with numpy

--- multi.py
+++ multi.py
@@ -1,43 +1,7 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn import linear_model
-import sklearn.metrics as sm
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt 
+import numpy
 
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
 
-df = pd.read_csv('co2.csv')
+x = numpy.percentile(ages, 90)
 
-print(df.head(10))
-print(df.describe())
-
-sns.countplot(x='out1', data=df)
-
-plt.subplots(figsize=(9, 9))
-sns.heatmap(df.corr(), annot=True)
-
-
-x = df.drop("out1", axis=1)
-x = x.drop("fuelcomb", axis=1)
-x = x.drop("cylandr", axis=1)
-y = df.out1
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-reg_linear= linear_model.LinearRegression()
-
-reg_linear.fit(X_train, y_train)
-
-y_test_pred = reg_linear.predict(X_test)
-
-print(X_test.size)
-
-test=np.array([[3]])
-
-khoroji = reg_linear.predict(test)
-
-print(khoroji)
-
-
-plt.show()
+print(x)
