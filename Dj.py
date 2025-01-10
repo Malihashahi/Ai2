7383dcb52d435f63d6e03b6774964d665beee48c
Malihashahi <maliha.shahi.03@gmail.com>
2025-01-10 22:49:26 +0330

    modify some code

--- Dj.py
+++ Dj.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Band(models.Model):
-    """A model of a rock band."""
+  
 
     name = models.CharField(max_length=200)
     can_rock = models.BooleanField(default=True)
 
 
 class Member(models.Model):
-    """A model of a rock band member."""
+  
 
     name = models.CharField("Member's name", max_length=200)
     instrument = models.CharField(
