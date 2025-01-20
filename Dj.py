d44d714ad5d73ffa19573e3e9b5e2827316e4cda
Malihashahi <maliha.shahi.03@gmail.com>
2025-01-20 22:50:36 +0330

    add link detils

--- Dj.py
+++ Dj.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
 
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
